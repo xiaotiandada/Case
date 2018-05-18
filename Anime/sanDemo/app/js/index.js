2c5c875a54eaa8009f07c890ae599381e9cc8794
xiaotiandada <952822399@qq.com>
2018-05-19 00:09:50 +0800

    san Anime End

--- Anime/sanDemo/app/js/index.js
+++ Anime/sanDemo/app/js/index.js
@@ -1,19 +1,39 @@
-var bodymovinLayer = document.getElementsByClassName('bodymovin')[0]
-var animDate = {
-    container: bodymovinLayer,
-    renderer: 'svg',
-    prerender:true,
-    loop: false,
-    autoplay: false,
-    path: bodymovinLayer.getAttribute('data-movpath')
-  }  
-anim = lottie.loadAnimation(animDate)
+$(function () {
+  var resourceCards = document.querySelectorAll('.resource-block')
+  var facilityCards = document.querySelectorAll('.facility-block')
+  var len = resourceCards.length
 
-$('.bodymovin').on('mouseenter',function(){
-  anim.play()
+  setBodymovin = function (cards, len) {
+    while (len--) {
+      var bodymovinLayer = cards[len].getElementsByClassName('bodymovin')[0]
+
+      var animData = {
+        container: bodymovinLayer,
+        renderer: 'svg',
+        prerender: true,
+        loop: false,
+        autoplay: false,
+        path: bodymovinLayer.getAttribute('data-movpath')
+      }
+
+      anim = lottie.loadAnimation(animData);
+
+      (function (anim) {
+        var card = cards[len]
+
+        $(card).on('mouseenter', function () {
+          anim.play()
+        })
+
+        $(card).on('mouseleave', function (e) {
+          anim.stop()
+        })
+      })(anim)
+
+    }
+
+  }
+
+  setBodymovin(resourceCards, len)
+  setBodymovin(facilityCards, len)
 })
-
-$('.bodymovin').on('mouseleave',function(e){
-  anim.stop()
-})
-
