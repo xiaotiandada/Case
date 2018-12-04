19572b5c54028705d4ea20cf6c08694c2698ca0d
_xiaotian <952822399@qq.com>
2018-12-04 23:14:52 +0800

    工厂模式 构造函数模式

--- book/js高级程序设计/6.js
+++ book/js高级程序设计/6.js
@@ -1,32 +1,119 @@
-var person = {}
-Object.defineProperty(person, 'name', {
-  writable: true,
-  value: 'Xiao'
-})
+{
+  var person = {}
+  Object.defineProperty(person, 'name', {
+    writable: true,
+    value: 'Xiao'
+  })
 
-// console.log(person.name);
-// person.name = 's'
-// console.log(person.name);
+  // console.log(person.name);
+  // person.name = 's'
+  // console.log(person.name);
 
 
-var book = {
-  _year: 2004,
-  edition: 1
+  var book = {
+    _year: 2004,
+    edition: 1
+  }
+
+  Object.defineProperty(book, 'year', {
+    get: function() {
+      return this._year
+    },
+
+    set: function(newValue){
+      if (newValue > 2004) {
+        this._year = newValue
+        this.edition += newValue - 2004
+      }
+    }
+  })
+
+
+  // book.year = 2005
+  // console.log(book.edition);
 }
 
-Object.defineProperty(book, 'year', {
-  get: function() {
-    return this._year
-  },
+// -------------------
 
-  set: function(newValue){
-    if (newValue > 2004) {
-      this._year = newValue
-      this.edition += newValue - 2004
-    }
+
+{
+  // 工厂模式
+function createPerson(name, age, job) {
+  var o = new Object()
+  o.name = name
+  o.age = age
+  o.job = job
+  o.sayName = function(){
+    console.log(this.name)
   }
-})
+  return o
+}
 
+var person1 = createPerson('xiao', 19, 'frontEnd')
+var person2 = createPerson('lei', 19, 'backEnd')
 
-book.year = 2005
-console.log(book.edition);
+// console.log(person1);
+// console.log(person2);
+}
+
+{
+  // 构造函数模式
+
+function Person(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = function(){
+    console.log(this.name)
+  }
+}
+
+var person1 = new Person('xiao', 19, 'frontEnd')
+var person2 = new Person('lei', 20, 'backEnd')
+
+// console.log(person1);
+// console.log(person2);
+
+// person1.sayName()
+// person2.sayName()
+
+// console.log(person1.constructor == Person);
+// console.log(person2.constructor == Person);
+
+// console.log(person1 instanceof Person);
+// console.log(person1 instanceof Object);
+// console.log(person2 instanceof Person);
+// console.log(person1 instanceof Object);
+}
+
+{
+  // 构造函数模式
+
+function Person(name, age, job) {
+  this.name = name
+  this.age = age
+  this.job = job
+  this.sayName = sayName
+}
+
+function sayName () {
+  console.log(this.name);
+}
+
+var person1 = new Person('xiao', 19, 'frontEnd')
+var person2 = new Person('lei', 20, 'backEnd')
+
+console.log(person1);
+console.log(person2);
+
+person1.sayName()
+person2.sayName()
+
+console.log(person1.constructor == Person);
+console.log(person2.constructor == Person);
+
+console.log(person1 instanceof Person);
+console.log(person1 instanceof Object);
+console.log(person2 instanceof Person);
+console.log(person1 instanceof Object);
+}
