a6f83186f9d7da0bfce63ad26cd77f02925350d9
_xiaotian <952822399@qq.com>
2018-12-06 23:30:01 +0800

    判断是否在可视区域内

--- book/js高级程序设计/6.js
+++ book/js高级程序设计/6.js
@@ -1,33 +1,31 @@
 {
-  var person = {}
-  Object.defineProperty(person, 'name', {
+  var person = {};
+  Object.defineProperty(person, "name", {
     writable: true,
-    value: 'Xiao'
-  })
+    value: "Xiao"
+  });
 
   // console.log(person.name);
   // person.name = 's'
   // console.log(person.name);
 
-
   var book = {
     _year: 2004,
     edition: 1
-  }
+  };
 
-  Object.defineProperty(book, 'year', {
+  Object.defineProperty(book, "year", {
     get: function() {
-      return this._year
+      return this._year;
     },
 
-    set: function(newValue){
+    set: function(newValue) {
       if (newValue > 2004) {
-        this._year = newValue
-        this.edition += newValue - 2004
+        this._year = newValue;
+        this.edition += newValue - 2004;
       }
     }
-  })
-
+  });
 
   // book.year = 2005
   // console.log(book.edition);
@@ -35,85 +33,113 @@
 
 // -------------------
 
-
 {
   // 工厂模式
-function createPerson(name, age, job) {
-  var o = new Object()
-  o.name = name
-  o.age = age
-  o.job = job
-  o.sayName = function(){
-    console.log(this.name)
+  function createPerson(name, age, job) {
+    var o = new Object();
+    o.name = name;
+    o.age = age;
+    o.job = job;
+    o.sayName = function() {
+      console.log(this.name);
+    };
+    return o;
   }
-  return o
-}
 
-var person1 = createPerson('xiao', 19, 'frontEnd')
-var person2 = createPerson('lei', 19, 'backEnd')
+  var person1 = createPerson("xiao", 19, "frontEnd");
+  var person2 = createPerson("lei", 19, "backEnd");
 
-// console.log(person1);
-// console.log(person2);
+  // console.log(person1);
+  // console.log(person2);
 }
 
 {
   // 构造函数模式
 
-function Person(name, age, job) {
-  this.name = name
-  this.age = age
-  this.job = job
-  this.sayName = function(){
-    console.log(this.name)
+  function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = function() {
+      console.log(this.name);
+    };
   }
-}
 
-var person1 = new Person('xiao', 19, 'frontEnd')
-var person2 = new Person('lei', 20, 'backEnd')
+  var person1 = new Person("xiao", 19, "frontEnd");
+  var person2 = new Person("lei", 20, "backEnd");
 
-// console.log(person1);
-// console.log(person2);
+  // console.log(person1);
+  // console.log(person2);
 
-// person1.sayName()
-// person2.sayName()
+  // person1.sayName()
+  // person2.sayName()
 
-// console.log(person1.constructor == Person);
-// console.log(person2.constructor == Person);
+  // console.log(person1.constructor == Person);
+  // console.log(person2.constructor == Person);
 
-// console.log(person1 instanceof Person);
-// console.log(person1 instanceof Object);
-// console.log(person2 instanceof Person);
-// console.log(person1 instanceof Object);
+  // console.log(person1 instanceof Person);
+  // console.log(person1 instanceof Object);
+  // console.log(person2 instanceof Person);
+  // console.log(person1 instanceof Object);
 }
 
 {
   // 构造函数模式
 
-function Person(name, age, job) {
-  this.name = name
-  this.age = age
-  this.job = job
-  this.sayName = sayName
+  function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+    this.sayName = sayName;
+  }
+
+  function sayName() {
+    console.log(this.name);
+  }
+
+  var person1 = new Person("xiao", 19, "frontEnd");
+  var person2 = new Person("lei", 20, "backEnd");
+
+  // console.log(person1);
+  // console.log(person2);
+
+  // person1.sayName()
+  // person2.sayName()
+
+  // console.log(person1.constructor == Person);
+  // console.log(person2.constructor == Person);
+
+  // console.log(person1 instanceof Person);
+  // console.log(person1 instanceof Object);
+  // console.log(person2 instanceof Person);
+  // console.log(person1 instanceof Object);
 }
 
-function sayName () {
-  console.log(this.name);
-}
+{
+  // 原型模式
+  function Person(){}
+  Person.prototype.name = 'xiao',
+  Person.prototype.age = 19,
+  Person.prototype.sayName = function(){
+    console.log(this.name);
+  }
 
-var person1 = new Person('xiao', 19, 'frontEnd')
-var person2 = new Person('lei', 20, 'backEnd')
+  var person1 = new Person()
+  person1.sayName()
 
-console.log(person1);
-console.log(person2);
+  var person2 = new Person()
+  person2.sayName()
 
-person1.sayName()
-person2.sayName()
+  person1.name = 'lei'
+  person1.sayName()
+  person2.sayName()
 
-console.log(person1.constructor == Person);
-console.log(person2.constructor == Person);
 
-console.log(person1 instanceof Person);
-console.log(person1 instanceof Object);
-console.log(person2 instanceof Person);
-console.log(person1 instanceof Object);
+
+  console.log(person1.hasOwnProperty('name'))
+  console.log(person2.hasOwnProperty('name'))
+
+  delete person1.name
+  person1.sayName()
+  person2.sayName()
 }
--- code/user.md
+++ code/user.md
@@ -65,3 +65,77 @@ function setMessage(text) {
     return hide();
 }
 ```
+
+## 判断是否在可视区域内
+> 需优化
+
+``` css
+
+.animation.active {
+  animation: slideInUp 600ms ease-in-out both;
+}
+
+.animation.remove,
+.head-text.animation.remove,
+.title.animation.remove {
+  animation: slideInDown 600ms ease-in-out both;
+}
+
+@keyframes slideInUp {
+  from {
+    transform: translate3d(0, 10%, 0);
+    visibility: visible;
+    opacity: 0;
+  }
+  to {
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+@keyframes slideInDown {
+  from {
+    transform: translate3d(0, 0, 0);
+  }
+  to {
+    transform: translate3d(0, 10%, 0);
+    visibility: visible;
+    opacity: 0;
+  }
+}
+```
+
+```js
+var activeAnimation = document.querySelectorAll('.animation')
+var len = activeAnimation.length
+window.addEventListener('scroll', function () {
+  isToggleAnimation(activeAnimation, len)
+})
+
+function isToggleAnimation(activeAnimation, len) {
+  while (len--) {
+    var boo = isElementInViewport(activeAnimation[len])
+    if (boo) {
+      if (activeAnimation[len].classList.contains('remove')) {
+        activeAnimation[len].classList.remove('remove')
+      }
+      activeAnimation[len].classList.add('active')
+    } else {
+      activeAnimation[len].classList.remove('active')
+      activeAnimation[len].classList.add('remove')
+    }
+  }
+}
+isToggleAnimation(activeAnimation, len)
+
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  // console.log(rect)
+  return (
+    (rect.top + rect.height) >= 0 &&
+    (rect.left + rect.width) >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+```
