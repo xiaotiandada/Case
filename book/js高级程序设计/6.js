8fd251c75d45da8e2eebc4f5e7fc3f574d85a721
_xiaotian <952822399@qq.com>
2018-12-20 22:40:59 +0800

    交互

--- book/js高级程序设计/6.js
+++ book/js高级程序设计/6.js
@@ -537,5 +537,20 @@
       return num * f(num - 1)
     }
   })
-  console.log(factorial(5))
+  // console.log(factorial(5))
+}
+
+{
+  function createFunctions() {
+    var result = new Array()
+    for (var i = 0; i < 10; i++) {
+      result[i] = function (num) {
+        return function () {
+          return num
+        }
+      }(i)
+    }
+    return result
+  }
+  console.log(createFunctions())
 }
