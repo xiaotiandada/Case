94996c33e42f92ea79af20d8c160e39f278696c8
_xiaotian <952822399@qq.com>
2018-12-29 17:46:18 +0800

    zepto

--- sourceCode/fun.js
+++ sourceCode/fun.js
@@ -20,5 +20,55 @@ const contains = document.documentElement.contains ?
     return false
   }
 
+
 // console.log(contains(document.querySelector('#test1'), document.querySelector('#test2')))
 // console.log(contains(document.querySelector('#test1'), document.querySelector('#test3')))
+
+// 如果object是array，则返回ture。
+const isArray = Array.isArray ||
+  function (obj) {
+    return obj instanceof Array
+  }
+// console.log(isArray([1]))
+
+// 自写 非 zepto.js
+const isFunction = obj => typeof obj == 'function'
+
+// console.log(isFunction(function () {}))
+
+// 如果该值为有限数值或一个字符串表示的数字，则返回ture。
+const isNumeric = val => {
+  let num = Number(val),
+    type = typeof val
+  return (
+    (val != null) &&
+    (type != 'boolean' &&
+      (type != 'string' || val.length) &&
+      !isNaN(num) &&
+      isFinite(num)) || false
+  )
+}
+
+// console.log(isNumeric(1), isNumeric('1'))
+
+// 如果object参数为一个window对象，那么返回true。
+const isWindow = obj => obj != null & obj == obj.window
+
+// 测试对象是否是“纯粹”的对象，这个对象是通过 对象常量（"{}"） 或者 new Object 创建的，如果是，则返回true。
+const isPlainObject = obj => {
+  return (
+    // 改
+    typeof obj == 'object' &&
+    !isWindow(obj) &&
+    Object.getPrototypeOf(obj) == Object.prototype
+  )
+}
+// console.log(isPlainObject({}))
+// console.log(isPlainObject(new Object))
+// console.log(isPlainObject(new Date))
+// console.log(isPlainObject(window))
+
+const trim = str => str == null ? '' : String.prototype.trim.call(str)
+
+// console.log(trim('1  1   1   '))
+// console.log(trim('   1  1   1   '))
