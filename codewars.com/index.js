341e6efe1d8bd6c06bf1f646413cd3c71510d73f
_xiaotian <952822399@qq.com>
2019-01-11 13:45:58 +0800

    You might know some pretty large perfect squares. But what about the NEXT one?

--- codewars.com/index.js
+++ codewars.com/index.js
@@ -15,20 +15,20 @@
 
   function list(names) {
     //your code here
-    const getName = obj => obj.name
+    const getName = obj => obj.name;
     const addStr = arr => {
-      let str = ''
-      let len = arr.length
-      if (len === 0) return str
-      else if (len === 1) return str = arr[0]
+      let str = "";
+      let len = arr.length;
+      if (len === 0) return str;
+      else if (len === 1) return (str = arr[0]);
       for (let i = 0; i < len; i++)
-        if (i === 0) str += arr[i]
-      else if (i === arr.length - 1) str += ' & ' + arr[i]
-      else str += ', ' + arr[i]
-      return str
-    }
-    const arr = names.map(getName)
-    return addStr(arr)
+        if (i === 0) str += arr[i];
+        else if (i === arr.length - 1) str += " & " + arr[i];
+        else str += ", " + arr[i];
+      return str;
+    };
+    const arr = names.map(getName);
+    return addStr(arr);
   }
 }
 
@@ -43,12 +43,36 @@
     The input string will only consist of lower case letters and/or spaces.
    */
   function getCount(str) {
-    const searchStr = str => str.match(/[aeiou]/g)
+    const searchStr = str => str.match(/[aeiou]/g);
 
-    const addCount = str => str ? str.length : 0
+    const addCount = str => (str ? str.length : 0);
 
-    const strs = searchStr(str)
+    const strs = searchStr(str);
 
     return addCount(strs);
+  }
+}
+
+{
+  /** 
+   * You might know some pretty large perfect squares. But what about the NEXT one?
+    Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+    If the parameter is itself not a perfect square, than -1 should be returned. You may assume the parameter is positive.
+
+    findNextSquare(121) --> returns 144
+    findNextSquare(625) --> returns 676
+    findNextSquare(114) --> returns -1 since 114 is not a perfect
+  */
+
+  function findNextSquare(sq) {
+    // Return the next square if sq if a perfect square, -1 otherwise
+
+    const sqrtNum = num => Math.sqrt(num);
+
+    const isInt = num => Number.isInteger(num);
+
+    const val = sqrtNum(sq);
+
+    return isInt(val) ? (val + 1) * (val + 1) : -1;
   }
 }
