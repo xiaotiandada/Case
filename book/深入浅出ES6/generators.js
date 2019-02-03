ce95b53befb799a7ed627de84c5249a47470fcec
_xiaotian <952822399@qq.com>
2019-02-03 22:55:09 +0800

    生成器

--- book/深入浅出ES6/generators.js
+++ book/深入浅出ES6/generators.js
@@ -11,12 +11,60 @@
   console.log(gen.next());
   console.log(gen.next());
 
-  let gen1 = generators('test')
+  let gen1 = generators("test");
   for (let i = 0; i < 10; i++) {
-    let obj = gen1.next()
+    let obj = gen1.next();
     if (obj.done) {
-      break
+      break;
     }
-    console.log(obj)
+    console.log(obj);
+  }
+}
+
+{
+  class RangeIterator {
+    constructor(start, stop) {
+      this.value = start;
+      this.stop = stop;
+    }
+
+    [Symbol.iterator]() {
+      return this;
+    }
+    next() {
+      let value = this.value;
+      if (value < this.stop) {
+        this.value++;
+        return {
+          value: value,
+          done: false
+        };
+      } else {
+        return {
+          value: undefined,
+          done: true
+        };
+      }
+    }
+  }
+
+  function range(start, stop) {
+    return new RangeIterator(start, stop);
+  }
+
+  for (let value of range(0, 3)) {
+    console.log(value);
+  }
+}
+
+{
+  for (const value of range(0, 3)) {
+    console.log(value);
+  }
+
+  function* range(start, stop) {
+    for (let i = start; i < stop; i++) {
+      yield i;
+    }
   }
 }
