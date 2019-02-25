0cda7fe6f8a3e1d04f9feb5f067886a899f9928b
_小田 <952822399@qq.com>
2019-02-25 16:14:12 +0800

    Update 防抖节流函数.js

--- code/防抖节流函数.js
+++ code/防抖节流函数.js
@@ -27,3 +27,24 @@ function test() {
 }
 
 window.addEventListener('scroll', throttle(test, 300))
+
+// ---
+
+const debounce = (fn: any, wait: number) => {
+  let timer: any = null;
+  return () => {
+    clearTimeout(timer);
+    timer = setTimeout(fn, wait);
+  };
+};
+
+const throttle = (fn: any, delay: number) => {
+  let lastTime: any = null;
+  return () => {
+    let nowTime = Date.now();
+    if (nowTime - lastTime >= delay) {
+      fn();
+      lastTime = nowTime;
+    }
+  };
+};
