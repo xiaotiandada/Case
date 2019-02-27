904b3e02adfea2ac9c52fd239057368fa9453d31
_小田 <952822399@qq.com>
2019-02-27 15:43:28 +0800

    两数之和
    两数之和

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -1,14 +1,12 @@
+// 1. 两数之和
 {
-  // 1.两数之和
-  /**
-   * @param {number[]} nums
-   * @param {number} target
-   * @return {number[]}
-   */
-  var twoSum = function (nums, target) {
-    let len = nums.length
-    for (let i = 0; i < len; i++)
-      for (let j = i + 1; j < len; j++)
-        if (nums[i] + nums[j] === target) return [i, j]
-  };
-}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0;i<nums.length;i++)
+        if (nums[i]+nums[i+1] === target)
+            return [i,i+1]
+};
