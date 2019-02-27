c0329d0ac9874eac8b260c054b2d918735fdcd4d
_小田 <952822399@qq.com>
2019-02-27 16:46:10 +0800

    7. 整数反转
    7. 整数反转

--- leetcode/leetcode.js
+++ leetcode/leetcode.js
@@ -1,5 +1,4 @@
 // 1. 两数之和
-{
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -9,4 +8,16 @@ var twoSum = function(nums, target) {
     for(let i = 0;i<nums.length;i++)
         if (nums[i]+nums[i+1] === target)
             return [i,i+1]
+};
+
+// 7. 整数反转
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let xStr = x+''
+    let booLeanStr = xStr.substr(0,1) === '-' 
+    let numStr = xStr.split('').reverse().join('')
+    return booLeanStr ? parseInt('-'+numStr) : parseInt(numStr) 
 };
