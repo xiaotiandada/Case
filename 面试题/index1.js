e5617d2e058e5e759d51b228d8bc92e96969e5a8
xiaotian <952822399@qq.com>
2020-02-21 01:33:43 +0800

    修复bug

--- 面试题/index1.js
+++ 面试题/index1.js
@@ -38,15 +38,41 @@ let lockMessageList = [
     messageTime: '2019-11-26 02:32:20'
   },
   {
+    message: '卡片开锁',
+    messageIcon: 3,
+    messageTime: '2019-11-26 04:32:20'
+  },
+  {
+    message: '卡片开锁',
+    messageIcon: 3,
+    messageTime: '2019-11-26 02:12:20'
+  },
+  {
     user: '妈妈',
     message: '指纹开锁',
     messageIcon: 1,
     messageTime: '2019-11-18 06:32:20'
   },
   {
-    message: '卡片开锁',
+    user: '妈妈',
+    message: '指纹开锁',
+    messageIcon: 1,
+    messageTime: '2019-11-18 06:22:20'
+  },
+  {
+    message: '卡片开锁2',
     messageIcon: 3,
     messageTime: '2019-10-26 01:32:10'
+  },
+  {
+    message: '卡片开锁1',
+    messageIcon: 3,
+    messageTime: '2019-10-26 11:11:11'
+  },
+  {
+    message: '卡片开锁',
+    messageIcon: 3,
+    messageTime: '2018-10-26 11:11:10'
   },
 ]
 
@@ -90,15 +116,22 @@ const dayGroup = arr => {
   // false x++ y = 0    改变x 重置y
   for(let i = 0; i < arr.length; i++) {
 
-    if (!dayArr[x]) dayArr[x] = []
-    dayArr[x][y] = arr[i]
-
     // 判断当前的时间是否和上一天相同
     if (isADay(currentTime, arr[i].messageTime)) {
+
+      // 如果没有数组则添加一个空数组
+      if (!dayArr[x]) dayArr[x] = []
+
+      // 如果从不同的天数切换过来,并且xy有数据则++
+      if (dayArr[x][y]) y++
+      dayArr[x][y] = arr[i]
       y++
     } else {
       x++
       y=0
+      // 如果没有数组则添加一个空数组
+      if (!dayArr[x]) dayArr[x] = []
+      dayArr[x][y] = arr[i]
     }
     currentTime = arr[i].messageTime
   }
@@ -109,11 +142,18 @@ const dayGroup = arr => {
 
 let timeSortList = timeSort(lockMessageList)
 let dayList = dayGroup(timeSortList)
-console.log(dayList)
+// console.log(dayList)
 
 var app = new Vue({
   el: '#app',
   data: {
     lockList: dayList
+  },
+  computed: {
+    lockListLength() {
+      let len = 0
+      this.lockList.forEach(el => len += el.length)
+      return len
+    }
   }
 })
